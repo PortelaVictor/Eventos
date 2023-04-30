7e57fe50219c20b98478f311b45978f290b93027
Victor Portela <portelavictor@hotmail.com>
2023-04-30 13:46:38 -0300

    Ef Core

---
+++ Back/src/Eventos.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Eventos.API.Models;
+
+namespace Eventos.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
